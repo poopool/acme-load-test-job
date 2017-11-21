c1ee1b9222cbec6e0bf56cf84cafa32466f8dd0d
pouya <pouya@applariat.com>
2017-11-20 17:20:58 -0800

    more work

--- run-test.sh
+++ run-test.sh
@@ -19,3 +19,40 @@ echo "fetching the list of cluster nodes:"
 kubectl get nodes
 check_status
 
+echo "locating acme-app dir"
+[ -d /usr/local/bin/kubectl ] || { echo "[!] ERROR: Missing acme-app dir. Aborting."; exit 1; }
+
+echo "deploying acme-app..."
+kubectl apply -f acme-app/
+check_status
+
+sleep 10
+
+echo "checking acme-app running status..."
+
+MONGO_STATUS=$(kubectl get po | grep mongo |awk '{print $3}')
+
+echo "MongoDB status: $MONGO_STATUS"
+if [ $MONGO_STATUS != "Running" ]
+then
+  while [ $MONGO_STATUS != "Running" ]
+  do
+    echo "waiting for MongoDB status to change to \"Running\""
+    wait 5
+  done
+fi
+
+NODE_STATUS=$(kubectl get po | grep node |awk '{print $3}')
+
+echo "Node.js status: $NODE_STATUS"
+if [ $NODE_STATUS != "Running" ]
+then
+  while [ $NODE_STATUS != "Running" ]
+  do
+    echo "waiting for Node.js status to change to \"Running\""
+    wait 5
+  done
+fi
+
+
+
