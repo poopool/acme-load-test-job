823ec7880b512b10d2fc45909f5c073d562daa52
pouya <pouya@applariat.com>
2017-11-27 12:53:09 -0800

    almost working...

--- run-test.sh
+++ run-test.sh
@@ -71,21 +71,26 @@ run_test()
         limit_mem+=Mi
         pod_count=$(echo $next |awk -F "," {'print $5'})
 
-        jq '.spec.template.spec.containers[].resources.requests.cpu = '$req_cpu'' acme-app/node-deploy.json |sponge acme-app/node-deploy.json
-        jq '.spec.template.spec.containers[].resources.requests.memory = '$req_mem'' acme-app/node-deploy.json |sponge acme-app/node-deploy.json
-        jq '.spec.template.spec.containers[].resources.limits.cpu = '$limit_cpu'' acme-app/node-deploy.json |sponge acme-app/node-deploy.json
-        jq '.spec.template.spec.containers[].resources.limits.memory = '$limit_mem'' acme-app/node-deploy.json |sponge acme-app/node-deploy.json
-        jq '.spec.replicas = '$pod_count'' acme-app/node-deploy.json |sponge acme-app/node-deploy.json
+        jq '.spec.template.spec.containers[].resources.requests.cpu = "'$req_cpu'"' acme-app/node-deploy.json \
+        | jq '.spec.template.spec.containers[].resources.requests.memory = "'$req_mem'"' acme-app/node-deploy.json \
+        | jq '.spec.template.spec.containers[].resources.limits.cpu = "'$limit_cpu'"' acme-app/node-deploy.json \
+        | jq '.spec.template.spec.containers[].resources.limits.memory = "'$limit_mem'"' acme-app/node-deploy.json \
+        | jq '.spec.replicas = '$pod_count'' > node-deploy-$req_cpu-$req_mem-$limit_cpu-$limit_mem.yaml
 
-        kubectl apply -f acme-app/node-deploy.json
+        kubectl apply -f node-deploy-$req_cpu-$req_mem-$limit_cpu-$limit_mem.yaml
         check_status
         sleep 10
 
         echo "starting ephemeral container to run tests inside it..."
         kubectl run exec-test  --image=nginx --env="BB_API_KEY=$BB_API_KEY" --env="req_cpu=$req_cpu" --env="req_mem=$req_mem" --env="limit_cpu=$limit_cpu" --env="limit_mem=$limit_mem"
         check_status
+        sleep 5
 
         EXEC_TEST_POD=$(kubectl get po |grep exec-test|awk '{print $1}')
+        echo "installing apache benchmark tool..."
+        kubectl exec -it $EXEC_TEST_POD -- bash -c "apt-get -qq update && apt-get -qq install -y apache2-utils wget curl zip"
+        echo "copying post file to remote container"
+        kubectl cp post.file ${EXEC_TEST_POD}:/post.file
 
         counter=1
         IFS=$'\n'
@@ -93,19 +98,19 @@ run_test()
         do
             c=$(echo $next |awk -F "," {'print $1'})
             n=$(echo $next |awk -F "," {'print $2'})
-            kubectl exec -it $EXEC_TEST_POD -- bash -c "apt-get update && apt-get install -y apache2-utils wget curl zip"
             echo "Running ab -k -c $c -n $n -T application/x-www-form-urlencoded -k -p post.file $URL &>test-$counter.txt"
-            kubectl exec -it $EXEC_TEST_POD -- bash -c "ab -k -c $c -n $n -T application/x-www-form-urlencoded -k -p post.file $URL &>test-$counter.txt"
+            kubectl exec -it $EXEC_TEST_POD -- bash -c "ab -k -c $c -n $n -T application/x-www-form-urlencoded -p post.file http://node:3000/rest/api/login &>test-$counter.txt"
             counter=$((counter+1))
         done
 
-        kubectl exec -it $EXEC_TEST_POD -- bash -c "export OUT_PUT_FILE=results-$req_cpu-$req_mem-$limit_cpu-$limit_mem"
-        kubectl exec -it $EXEC_TEST_POD -- bash -c "mkdir -p $OUT_PUT_FILE"
-        kubectl exec -it $EXEC_TEST_POD -- bash -c "mv test-* $OUT_PUT_FILE"
-        kubectl exec -it $EXEC_TEST_POD -- bash -c "zip -r $OUT_PUT_FILE.zip $OUT_PUT_FILE"
+        OUT_PUT_FILE=results-$req_cpu-$req_mem-$limit_cpu-$limit_mem
+
+        kubectl exec -it $EXEC_TEST_POD -- bash -c "mkdir -p results"
+        kubectl exec -it $EXEC_TEST_POD -- bash -c "mv test-* results"
+        kubectl exec -it $EXEC_TEST_POD -- bash -c "zip -r $OUT_PUT_FILE.zip results"
 
         echo "Uploading results to bitbucket storage"
-        kubectl exec -it $EXEC_TEST_POD -- bash -c "curl --user applariat:$BB_API_KEY --form files=@"${OUT_PUT_FILE}" "https://api.bitbucket.org/2.0/repositories/applariat/apl-policy/downloads""
+        kubectl exec -it $EXEC_TEST_POD -- bash -c "curl --user applariat:$BB_API_KEY --form files=@"${OUT_PUT_FILE}.zip" "https://api.bitbucket.org/2.0/repositories/applariat/apl-policy/downloads""
         echo "killing ephemeral container"
         kubectl delete deploy exec-test
 
@@ -140,48 +145,10 @@ sleep 10
 
 echo "checking acme-app running status..."
 sleep 1
-#
-#MONGO_STATUS=$(kubectl get po | grep mongo |awk '{print $3}')
-#MONGO_POD_NAME=$(kubectl get po | grep mongo |awk '{print $1}')
-#
-#echo "MongoDB status: $MONGO_STATUS"
-#sleep 1
-#if [ $MONGO_STATUS != "Running" ]
-#then
-#  while [ $MONGO_STATUS != "Running" ]
-#  do
-#    echo "waiting for MongoDB status to change to \"Running\""
-#    sleep 5
-#    MONGO_STATUS=$(kubectl get po | grep mongo |awk '{print $3}')
-#  done
-#fi
 
 mongo_status
 
-#NODE_STATUS=$(kubectl get po | grep node |awk '{print $3}')
-#NODE_POD_NAME=$(kubectl get po | grep node |awk '{print $1}')
-#
-#echo "Node.js status: $NODE_STATUS"
-#sleep 1
-#if [ $NODE_STATUS != "Running" ]
-#then
-#  while [ $NODE_STATUS != "Running" ]
-#  do
-#    echo "waiting for Node.js status to change to \"Running\""
-#    sleep 5
-#    NODE_STATUS=$(kubectl get po | grep node |awk '{print $3}')
-#  done
-#fi
-
 node_status
-
-#NODE_EXT_IP=$(kubectl get svc |grep node |awk '{print $3}')
-#while [ $NODE_EXT_IP = "<pending>" ]
-#do
-#    echo "waiting for node component public IP..."
-#    sleep 5
-#    NODE_EXT_IP=$(kubectl get svc |grep node |awk '{print $3}')
-#done
 
 get_node_ext-ip
 
@@ -192,6 +159,7 @@ CONN_STAT=$(nc -z $NODE_EXT_IP 3000; echo $?)
 delay_counter=12
 while [ $CONN_STAT -ne 0 ] && [ $delay_counter -ne 0 ]
 do
+    echo "acme-app is not up yet, going to sleep for 10 seconds"
     sleep 10
     delay_counter=$((delay_counter-1))
     CONN_STAT=$(nc -z $NODE_EXT_IP 3000; echo $?)
